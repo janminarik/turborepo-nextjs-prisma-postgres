ed7a2ade95ea7a05541ade34d81a6f1e74e62caa
luannguyenbkit <luannguyenbkit@gmail.com>
2024-12-08 12:19:22 +0700

    feat: update post

--- apps/web/app/(public-fullwidth)/author/[authorId]/page.tsx
+++ apps/web/app/(public-fullwidth)/author/[authorId]/page.tsx
@@ -14,7 +14,12 @@ export default async function Page({ params }: { params: { authorId: string } })
     <div className="grid grid-cols-12 gap-10">
       <UserProfile author={author} />
       <div className="col-span-8 rounded-md">
-        {author?.post?.map((post) => <PostItem key={post?.id} post={post} />)}
+        {author?.post?.map((post) => (
+          <PostItem
+            key={post?.id}
+            post={post}
+          />
+        ))}
       </div>
     </div>
   )
--- packages/database/src/prisma.ts
+++ packages/database/src/prisma.ts
@@ -24,5 +24,4 @@ if (process.env.NODE_ENV === "production") {
   prisma = global.prisma
 }
 
-
 export default prisma
